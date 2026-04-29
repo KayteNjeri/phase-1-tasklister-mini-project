c2c9f0dbe812a4a1d6fdd67d273df1d4f6daf20d
Catherine <canjeri91@gmail.com>
2026-04-30 00:26:14 +0300

    TaskLister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // add event listener to form
+  const form = document.querySelector("#create-task-form");
+
+	function handleSubmit(event){ 
+		event.preventDefault(); //add preventDefault()
+		const taskInput = event.target["new-task-description"].value; //select the correct target from the event and saving it as taskInput
+
+		const newTask = { //object
+			description: taskInput
+		};	
+		buildToDo(newTask); //call the function
+		event.target.reset(); //clear the input
+		//console.log("Task has been submitted");
+		//console.log(newTask);
+	};
+
+	form.addEventListener("submit", (event) => { //add submit for type of event and add arrow function
+		handleSubmit(event);
+
+	});
+
+	function buildToDo(task) {
+		const taskList = document.querySelector("#tasks");
+		const newTask = document.createElement("li"); //create the new li element
+
+		newTask.textContent = task.description; //add the task as the textContent
+
+		taskList.append(newTask); //append the li element to tasklist
+	}
 });
+
